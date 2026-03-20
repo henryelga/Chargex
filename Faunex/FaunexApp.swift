f411a1e9c9cedb2462315906195e4b1a9e3f47ab
Elga JH <d00262370@student.dkit.ie>
2026-03-20 12:29:07 +0000

    Initial Commit

---
+++ Faunex/FaunexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaunexApp.swift
+//  Faunex
+//
+//  Created by Student on 20/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FaunexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaunexTests/FaunexTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaunexTests.swift
+//  FaunexTests
+//
+//  Created by Student on 20/03/2026.
+//
+
+import Testing
+@testable import Faunex
+
+struct FaunexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

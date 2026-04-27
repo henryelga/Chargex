0666e7af3e840f760f74fa5bcc8c6a200c39dc4c
Mila <d00253640@student.dkit.ie>
2026-04-27 11:57:07 +0100

    Add toggle section on settings page to enable notifications

--- Faunex/Views/Settings/SettingsScreen.swift
+++ Faunex/Views/Settings/SettingsScreen.swift
@@ -4,6 +4,8 @@ struct SettingsScreen: View {
     
     @AppStorage("isDarkMode") private var isDarkMode = false
     @AppStorage("isLargeText") private var isLargeText = false
+    @AppStorage("notificationsEnabled") private var notificationsEnabled = false
+    @AppStorage("notificationDelay") private var notificationDelay = 10
     
     var body: some View {
         NavigationView {
@@ -20,6 +22,17 @@ struct SettingsScreen: View {
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
+                
+                Section(header: Text("Charging Notifications")) {
+                                    
+                                    Toggle("Enable Notifications", isOn: $notificationsEnabled)
+                                    
+                                    if notificationsEnabled {
+                                        Stepper(value: $notificationDelay, in: 1...120) {
+                                            Text("Notify after \(notificationDelay) minutes")
+                                        }
+                                    }
+                                }
             }
             .navigationTitle("Settings")
         }
